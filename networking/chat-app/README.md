705a7233c78d5c68ec85d20284acca456e289540
leonatajeff <jeffleonata@gmail.com>
2025-07-28 11:09:44 -0400

    networking pt 2

---
+++ networking/chat-app/README.md
@@ -0,0 +1,5 @@
+Intra Process Communication (OS concept but still applicable to networking)
+- if we do `server.list(path...)` it is for IPC servers (replace place with port for TCP servers)
+We'll be using the net module for reference: https://nodejs.org/api/net.html
+
+
---
+++ networking/chat-app/server.js
@@ -0,0 +1,12 @@
+const net = require("node:net");
+
+const server = net.createServer();
+
+server.on("connection", (socket) => {
+    console.log("A new connection to the server");
+    
+});
+
+server.listen(3008, "127.0.0.1", () => {
+    console.log("opened server on", server.address());
+});
